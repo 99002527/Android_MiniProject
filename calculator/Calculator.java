c5681bfd9fe98204def9863490972d1dca562995
99002493 <71540923+99002493@users.noreply.github.com>
2020-11-12 16:17:58 +0530

    Update Calculator.java

--- calculator/Calculator.java
+++ calculator/Calculator.java
@@ -9,6 +9,12 @@ import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
